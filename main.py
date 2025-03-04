8ef465604227f8b0d0ba310ae8007329c23c33d2
gody10 <odycaptain@gmail.com>
2025-03-04 14:06:57 -0700

    Finish player tracker and reuse results

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from utils import (read_video, save_video) 
+from trackers import PlayerTracker
 
 def main():
+    # Read video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
 
-    save_video(video_frames, "output_videos/output_video.avi")
+    # Initialize player tracker
+    player_tracker = PlayerTracker(model_path= "yolo11n")
+
+    # Detect players in the video
+    player_detections = player_tracker.detect_frames(frames= video_frames, read_from_stub= True, stub_path= "tracker_stubs/player_detections.pkl")
+
+    # Draw output
+
+    ## Draw player bounding boxes
+    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
     main()
