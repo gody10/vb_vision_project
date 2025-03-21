59170b21698433deec951116683439485676cfc2
gody10 <odycaptain@gmail.com>
2025-03-21 14:22:32 -0700

    Add MiniCourt Box On Video

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import (read_video, save_video) 
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+from mini_court import MiniCourt
 import cv2
 
 def main():
@@ -32,8 +33,10 @@ def main():
     # Choose and filter players
     player_detections = player_tracker.choose_and_filter_players(court_keypoints, player_detections, n_of_players= 2)
 
-    # Draw output
+    # MiniCourt
+    mini_court = MiniCourt(video_frames[0])
 
+    # Draw output
     ## Draw player bounding boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
 
@@ -42,6 +45,9 @@ def main():
 
     ## Draw court lines
     output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
+
+    # Draw Mini Court
+    output_video_frames = mini_court.draw_mini_court(output_video_frames)
 
     ## Draw frame number on top left corner
     for i, frame in enumerate(output_video_frames):
